c9f10e441f0cec53b2e70d55205ccd5ce209f214
Stagomon <stagomon@gmail.com>
2024-10-08 00:57:29 +0000

    small fixes

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,9 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("main_page.html")
 
