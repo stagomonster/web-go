a6266763941a98d90303dbe462d8ebce1bcb9e0b
Stagomon <stagomon@gmail.com>
2024-10-13 04:11:43 +0000

    added files

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,16 @@ from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{stagomon}:{commentsdb}@{hostname}/{databasename}".format(
+    username="the username from the 'Databases' tab",
+    password="the password you set on the 'Databases' tab",
+    hostname="the database host address from the 'Databases' tab",
+    databasename="the database name you chose, probably yourusername$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 comments = []
 
